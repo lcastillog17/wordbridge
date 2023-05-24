0087d575524d8d1b93bc47ecb51c52a5f90e9e83
AntonioBarreraUMG <79070136+AntonioBarreraUMG@users.noreply.github.com>
2023-05-23 22:33:17 -0600

    Delete WordBridgeCustomTranslator.java

--- src/main/java/com/compiladores/wordbridge/translator/WordBridgeCustomTranslator.java
+++
@@ -1,9 +0,0 @@
-package com.compiladores.wordbridge.translator;
-
-public interface WordBridgeCustomTranslator {
-	
-	public String translateStatement(String statement);
-	
-	public String translateQuestion(String question);
-
-}
